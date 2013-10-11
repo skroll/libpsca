18d90397151773e95b9780a73302bbc21d0095c9
Scott M. Kroll <skroll@gmail.com>
2013-10-11 12:51:13 -0400

    Add documentation generation
    * Add "doc" target for generating documentation with doxygen.
    * Update header doxygen comments.

--- lib/psca.h
+++ lib/psca.h
@@ -41,23 +41,83 @@ int psca_version_patch(void);
 /**
  * @brief Memory allocation function pointer.
  *
- * @param[in,out]  size
- * @param[in,out]  offset
- * @param[in]      context
+ * This prototype describes a callback method used by the pool to allocate
+ * memory. It allows for memory to be allocated from any source to be used
+ * by the pool. `size` is passed as a pointer since the allocator is allowed
+ * to allocate more than the requested size.
+ *
+ * Here is an example of an allocator that scribbles a value at the beginning
+ * of a block:
+ * @code
+ *     void * my_alloc_func(size_t *size, void *context)
+ *     {
+ *         size_t sz = *size;
+ *         uint32_t magic = 0xDEADBEEF;
+ *         void *block = malloc(sz + sizeof(magic));
+ *
+ *         if (block == NULL) return NULL;
+ *
+ *         memcpy(block, &magic, sizeof(magic));
+ *
+ *         *size = sz;
+ *
+ *         return block + sizeof(magic);
+ *     }
+ * @endcode
+ *
+ * @param[in,out]  size     Number of bytes to allocate. On return, the
+ *                          function should set the actual number of bytes
+ *                          allocated that are usable by the caller.
+ *
+ * @param[in]      context  User data set by psca_set_funcs().
+ *
+ * @return                  Pointer to newly allocated block of memory, or
+ *                          NULL on error.
+ *
+ * @see psca_set_funcs()
+ * @see psca_free_func_t
  */
 typedef void * (* psca_alloc_func_t)(size_t *size, void *context);
 
 /**
  * @brief Memory deallocation function pointer.
  *
- * @param[in]  block
- * @param[in]  offset
- * @param[in]  context
+ * This prototype describes a callback method used by the pool to deallocate
+ * memory that was allocated by the psca_alloc_func_t callback.
+ *
+ * Here is an example of a deallocator that checks a value prepended to the
+ * beginning of a block.
+ *
+ * @code
+ *     void my_free_func(void *block, void *context)
+ *     {
+ *         void *p = block - sizeof(uint32_t);
+ *         uint32_t magic;
+ *
+ *         memcpy(&magic, p, sizeof(magic));
+ *
+ *         if (magic != 0xDEADBEEF) {
+ *             .... log some error ....
+ *         }
+ *
+ *         free(p);
+ *     }
+ * @endcode
+ *
+ * @param[in]  block    Block to free.
+ *
+ * @param[in]  context  User data set by psca_set_funcs().
+ *
+ * @see psca_set_funcs()
+ * @see psca_alloc_func_t
  */
 typedef void (* psca_free_func_t)(void *block, void *context);
 
 /**
  * @brief Handle for a psca pool.
+ *
+ * This is a simple opaque pointer used for all functions that access
+ * or modify the pool.
  */
 typedef const void * psca_t;
 
@@ -65,6 +125,8 @@ typedef const void * psca_t;
  * @brief Initialize a new pool.
  *
  * @return              New pool or NULL on error.
+ *
+ * @see psca_destroy()
  */
 psca_t psca_new(void);
 
@@ -76,6 +138,8 @@ psca_t psca_new(void);
  * @return              Returns 0 on success and -1 on error. A possible
  *                      failure condition is if the pool still had frames
  *                      on the stack when it was being destroyed.
+ *
+ * @see psca_new()
  */
 int psca_destroy(psca_t pool);
 
@@ -83,13 +147,19 @@ int psca_destroy(psca_t pool);
  * @brief Set allocation/deallocation functions for a pool.
  *
  * @param[in]  pool       The pool to set the functions for.
+ *
  * @param[in]  alloc_func The allocation function to use.
+ *
  * @param[in]  free_func  The deallocation function to use.
+ *
  * @param[in]  context    User data supplied in callback.
  *
  * @warning               Do not change allocation functions if anything
  *                        has been allocated from the pool, it will result
  *                        in undefined (and probably unstable) behavior.
+ *
+ * @see psca_alloc_func_t
+ * @see psca_free_func_t
  */
 void psca_set_funcs(psca_t pool, psca_alloc_func_t alloc_func,
                     psca_free_func_t free_func, void *context);
@@ -97,32 +167,61 @@ void psca_set_funcs(psca_t pool, psca_alloc_func_t alloc_func,
 /**
  * @brief Set block size for a pool.
  *
- * @param[in]  pool
- * @param[in]  value
+ * The block size is the default size for block allocations by the pool. It
+ * is ideal to set this to a value large enough to hold memory for
+ * several frames.
+ *
+ * @param[in]  pool     The pool to set the block size for.
+ *
+ * @param[in]  value    The size (in bytes) to set the block size to.
  */
 void psca_set_block_size(psca_t pool, size_t value);
 
 /**
  * @brief Set the growth factor for a pool.
  *
- * @param[in]  pool
- * @param[in]  value
+ * The growth factor is used when an allocation greater than the block size
+ * for the pool is requested. When such an allocation is requested, the size
+ * is multiplied by the growth factor before the block is allocated, so that
+ * further allocations will not require a new block.
+ *
+ * @param[in]  pool     The pool to set the growth factor for.
+ *
+ * @param[in]  value    The growth factor.
  */
 void psca_set_growth_factor(psca_t pool, int value);
 
 /**
  * @brief Push a new frame onto the pool allocation stack.
  *
- * @param[in]  pool
+ * A frame is pushed in the pool when the context of memory allocations
+ * should change. All memory allocated from the pool will do so within the
+ * context of the top-most frame. When the frame is popped off the stack,
+ * it's allocations are released.
  *
- * @return              Pointer to newly pushed frame.
+ * @param[in]  pool     The pool to push the frame onto.
+ *
+ * @return              Pointer to newly pushed frame. This can be used to
+ *                      verify the return value in psca_pop() to check for
+ *                      stack imbalances.
+ *
+ * @see psca_pop()
  */
 const void *psca_push(psca_t pool);
 
 /**
  * @brief Pop a frame from the pool allocation stack.
  *
- * @return              Pointer to popped frame.
+ * A frame the is popped will have all of it's allocations released. If the
+ * frame caused any blocks to be allocated, those will be freed as well.
+ *
+ * @param[in]  pool     The pool to pop a frame from.
+ *
+ * @return              Pointer to popped frame. This can be used to verify
+ *                      the value returned by psca_push() to check for stack
+ *                      imbalances.
+ *
+ * @see psca_push()
  */
 const void *psca_pop(psca_t pool);
 
@@ -130,6 +229,7 @@ const void *psca_pop(psca_t pool);
  * @brief Allocate memory from the pool allocation stack.
  *
  * @param[in]  pool     The pool to allocate from.
+ *
  * @param[in]  size     Number of bytes to allocate.
  *
  * @return              Allocated memory, NULL on error.
